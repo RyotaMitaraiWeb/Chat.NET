a83dfabbe03375010757213bb5c70c4b6a33466f
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-02 18:02:02 +0200

    add normalize.css

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   const [count, setCount] = useState(0);
   return (
     <main>
+      <p>test</p>
       The count is {count}
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </main>
