cf13ce4337f7f859736a169798b05c421d4ffce5
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-03 23:18:16 +0200

    attempt to build a docker container for integration tests

--- server/Chat.NET/Tests.Integration/UnitTest1.cs
+++ server/Chat.NET/Tests.Integration/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace Tests.Integration
 {
     public class Tests
@@ -11,6 +13,15 @@ namespace Tests.Integration
         public void Test1()
         {
             Assert.Pass();
+        }
+
+        [Test]
+        public async Task Test2()
+        {
+            var client = new HttpClient();
+            var res = await client.GetAsync("http://localhost:5000/WeatherForecast");
+            Assert.That(res.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
         }
     }
 }
