b8eaa20662dbcc2a3be31e4273530e8352d239e2
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-05 17:24:18 +0200

    add a sample inmemory test

--- server/Chat.NET/Tests.Integration/UnitTest2.cs
+++ server/Chat.NET/Tests.Integration/UnitTest2.cs
@@ -1,4 +1,7 @@
 ﻿using Chat.NET;
+using Infrastructure.Postgres.Entities;
+using Infrastructure.Postgres.Repository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,13 +15,15 @@ namespace Tests.Integration
     {
         public IntegrationTestWebAppFactory<Program> factory;
         public HttpClient client;
-
+        public InMemoryDB testDb = new();
+        public Repository repository;
 
         [SetUp]
         public async Task SetUp()
         {
             this.factory = new IntegrationTestWebAppFactory<Program>();
             this.client = this.factory.CreateClient();
+            this.repository = testDb.Repository;
         }
 
         [TearDown]
@@ -47,6 +52,24 @@ namespace Tests.Integration
         {
             var res = await this.client.GetAsync("/WeatherForecast/Test2");
             Assert.That(res.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+        }
+
+        [Test]
+        public async Task InMemoryTest()
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = "test125",
+                NormalizedUserName = "TEST125",
+                PasswordHash = "te2!Amla;sqw"
+            };
+
+            await repository.AddAsync(user);
+
+            await repository.SaveChangesAsync();
+
+            var test = await repository.All<ApplicationUser>().ToListAsync();
+            Assert.That(test, Has.Count.EqualTo(1));
         }
     }
 }
