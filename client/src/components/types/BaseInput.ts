9bbb56948e22ee479f98215bcbfcd79ff5b37ab3
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-08 00:14:40 +0200

    create BaseInput interface

---
+++ client/src/components/types/BaseInput.ts
@@ -0,0 +1,11 @@
+import { HTMLInputTypeAttribute } from 'react';
+import { ElementProps } from './Element';
+
+export type inputType = HTMLInputTypeAttribute;
+
+/** Props representing the standard ``input`` attributes applicable to all variants */
+export interface BaseInputProps extends ElementProps {
+  name?: string;
+  disabled?: boolean;
+  form?: string;
+}
