8158a4b3580c58336b258e4aeb579b8df0bca97b
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-11 22:27:58 +0200

    create Box component

---
+++ client/src/components/box/Box.tsx
@@ -0,0 +1,18 @@
+import { createElement } from 'react';
+import { BoxProps } from './types';
+import './Box.scss';
+
+/**
+ * A standard wrapper that includes an accent background and a shadow.
+ */
+function Box(props: BoxProps): React.JSX.Element {
+  const { className = '', selector = 'div', ...others } = props;
+  const element = createElement(selector as string, {
+    className: 'component-box shadow ' + className,
+    ...others,
+  });
+
+  return element;
+}
+
+export default Box;
