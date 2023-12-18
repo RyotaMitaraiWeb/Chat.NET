f8ef7b624980ba0a3bb7528c402f86f23c3834f3
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-18 15:55:00 +0200

    integrate icons into the list item components

--- client/src/components/list/listItemButton/ListItemButton.tsx
+++ client/src/components/list/listItemButton/ListItemButton.tsx
@@ -7,18 +7,33 @@ import BaseButton from '@/components/internal/baseButton/BaseButton';
  * Can be turned into a hyperlink.
  */
 function ListItemButton(props: ListItemButtonProps): React.JSX.Element {
-  const { className = '', children, href, target, onClick, disabled, ...others } = props;
+  const {
+    className = '',
+    inset,
+    children,
+    href,
+    target,
+    onClick,
+    disabled,
+    icon,
+    ...others
+  } = props;
 
   return (
-    <li className={`component-list-item-button ${className}`} {...others}>
+    <li
+      className={`component-list-item-button ${icon || inset ? 'with-icon' : ''} ${className}`}
+      {...others}
+    >
       <BaseButton
         className="button"
         disabled={disabled}
         onClick={onClick}
         href={href}
         target={target}
+        icon={icon}
       >
-        {children}
+        <div className={`component-list-item-icon ${inset ? 'inset' : ''}`}>{icon}</div>
+        <div className="content">{children}</div>
       </BaseButton>
     </li>
   );
--- client/src/components/types/BaseListItem.ts
+++ client/src/components/types/BaseListItem.ts
@@ -2,4 +2,10 @@ import { ElementProps } from './Element';
 
 export interface BaseListItemProps extends ElementProps {
   children: React.ReactNode;
+  /**
+   * Use this when some of the list items have icons, but others do not.
+   * This property will align the non-icon items the same way as the icon
+   * ones.
+   */
+  inset?: boolean;
 }
