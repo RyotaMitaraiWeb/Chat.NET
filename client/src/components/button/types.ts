c479e0f67998bc24eec6a0fa20391af5ca626a84
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-18 19:22:30 +0200

    remove unneeded types

--- client/src/components/button/types.ts
+++ client/src/components/button/types.ts
@@ -1,18 +1,7 @@
-import { HTMLAttributeAnchorTarget } from 'react';
 import { BaseButtonWithIconProps } from '../types/BaseButton';
 import { palette, size } from '../types/options';
 
 export interface ButtonProps extends BaseButtonWithIconProps {
-  /** Passing this property to the element with a valid value
-   * will turn it into a <Link> that is styled like a button
-   */
-  href?: string;
-
-  /**
-   * If equal to ``_blank`` and passed alongside a valid ``href``,
-   * this will turn the element into an ``<a>`` that is styled like a button
-   */
-  target?: HTMLAttributeAnchorTarget;
   color?: palette;
   size?: size;
 }
