c74b03f1c62971ef825f2ce80b295bd02ad034ea
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-19 13:04:36 +0200

    document Dialog usage further

--- client/src/components/dialog/Dialog.tsx
+++ client/src/components/dialog/Dialog.tsx
@@ -10,10 +10,20 @@ import { findFirstFocusableElement } from '../internal/overlay/findFirstFocusabl
  * Conveys information to the user while disrupting their work.
  * The dialog blocks scrolling in the page (though you can scroll in the
  * dialog itself if its content overflows vertically),
- * but can be closed by clicking outside it
+ * but can be closed by clicking outside it, pressing the Escape key,
  * and potentially through buttons that are passed to it
  * and implement such logic. Note that the dialog is mounted
  * directly in the body via a portal.
+ *
+ * When the dialog is opened,
+ * the first focusable element within it will receive focus, if
+ * such exists.
+ *
+ * **Accessibility notes:** you should always pass an appropriate ``role``
+ * (e.g. ``alertdialog``) when using this component. In addition,
+ * if the dialog can be opened explicitly by the user (e.g. by clicking a button),
+ * you should return the focus to the element that opened the dialog upon
+ * closing.
  */
 function Dialog(props: DialogProps): React.JSX.Element {
   const { className = '', children, open, onClose, ...others } = props;
