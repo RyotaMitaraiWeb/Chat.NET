30220c992dac7f4bdb42f769327f331669bc4309
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-21 20:33:23 +0200

    make bolding optional for list items

--- client/src/components/list/listItem/ListItem.tsx
+++ client/src/components/list/listItem/ListItem.tsx
@@ -5,10 +5,12 @@ import '../ListItem.scss';
  * A standard list item to be used as a child of a ``<List>`` component
  */
 function ListItem(props: ListItemProps): React.JSX.Element {
-  const { className = '', inset, children, icon, ...others } = props;
+  const { className = '', inset, bold, children, icon, ...others } = props;
   return (
     <li
-      className={`component-list-item ${icon || inset ? 'with-icon' : ''} ${className}`}
+      className={`component-list-item ${bold ? 'bolded' : ''} ${
+        icon || inset ? 'with-icon' : ''
+      } ${className}`}
       {...others}
     >
       <div className={`component-list-item-icon ${inset ? 'inset' : ''}`}>{icon}</div>
