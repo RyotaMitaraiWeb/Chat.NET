7f34a8cd1c69b4a7d7fc5b9aaf33a94477fe36c8
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-22 21:13:58 +0200

    update documentation

--- client/src/components/dialog/Dialog.tsx
+++ client/src/components/dialog/Dialog.tsx
@@ -2,25 +2,13 @@ import { DialogProps } from './types';
 import './Dialog.scss';
 import BaseModal from '../internal/baseModal/BaseModal';
 /**
- * Conveys information to the user while disrupting their work.
- * The dialog blocks scrolling in the page (though you can scroll in the
- * dialog itself if its content overflows vertically),
- * but can be closed by clicking outside it, pressing the Escape key,
- * and potentially through buttons that are passed to it
- * and implement such logic. Note that the dialog is mounted
- * directly in the body via a portal.
- *
- * The dialog handles the automatic focusing on opening and closing, as
- * well as focus trapping.
+ * Conveys information to the user while disrupting their work. Can be
+ * used to present content or provide possible actions to the user.
  *
  * **Usage notes:** you may want to use the ``<DialogTitle>``, ``<DialogComponent>``,
  * and ``<DialogFooter>`` components to better describe the content
  * of your dialog. Some of those also come with some functionalities
- * and styling. You also need to have at least one focusable element
- * within ``children`` (such as a button or a hyperlink).
- *
- * **Accessibility notes:** you should always pass an appropriate ``role``
- * (e.g. ``alertdialog``) when using this component.
+ * and styling.
  */
 function Dialog(props: DialogProps): React.JSX.Element {
   const { className = '', children, open, onClose, ...others } = props;
--- client/src/components/internal/baseModal/BaseModal.tsx
+++ client/src/components/internal/baseModal/BaseModal.tsx
@@ -5,6 +5,31 @@ import Overlay from '../overlay/Overlay';
 import { createPortal } from 'react-dom';
 import { useCallback, useEffect, useState } from 'react';
 
+/**
+ * A base component for implementing modal components. Each modal:
+ * * comes with an overlay and a ``<Box>`` container
+ * * can be closed by clicking the overlay, pressing the Escape key,
+ * and by changing the state that maintains the modal.
+ * blocks interactions with other elements of the page while the modal
+ * is opened.
+ * * animates the opening and closing in a consistent manner regardless
+ * of the method of opening or closing. The animations occur only
+ * if the user's system allows it. For users that do not want animations,
+ * there will be a small delay that lasts less than a second where they will be unable
+ * to interact with the browser despite the modal and overlay having closed abruptly.
+ *
+ * * * The animation for the overlay consists of fading in and fading out, wheres
+ * the animation of the model itself is left to a more specific component. The
+ * implementing component's root selector will receive the ``closed`` class
+ * which you can use to trigger a closing animation. Until then, the root
+ * selector will have the class ``open``.
+ * * implements a focus trap and automatically focuses the first focusable
+ * element
+ *
+ * Each modal must have at least one focusable item (e.g. a hyperlink or a button)
+ * within it. You should also provide an appropriate ``role`` for your modal
+ * (e.g. ``alertdialog``).
+ */
 function BaseModal(props: BaseModalProps): React.JSX.Element {
   const { className = '', children, open, onClose, ...others } = props;
   const [isOpen, setOpen] = useState(open);
