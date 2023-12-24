885c7afea06dc732332143dabdd55a30e3a22c73
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-24 23:47:23 +0200

    create Drawer component

--- client/src/components/types/options.ts
+++ client/src/components/types/options.ts
@@ -1,2 +1,7 @@
 export type palette = 'primary' | 'secondary' | 'error' | 'warning' | 'success';
 export type size = 'small' | 'medium' | 'large';
+
+/**
+ * Unless stated otherwise, defaults to ``left``.
+ */
+export type side = 'left' | 'right' | 'top' | 'bottom';
