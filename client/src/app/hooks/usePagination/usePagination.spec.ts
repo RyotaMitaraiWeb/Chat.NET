ba624d8c90603269114b6ef51c2e2d415c501668
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-26 19:51:50 +0200

    create usePagination hook

---
+++ client/src/app/hooks/usePagination/usePagination.spec.ts
@@ -0,0 +1,17 @@
+import { act, renderHook } from '@testing-library/react';
+import { usePagination } from './usePagination';
+
+describe('usePagination', () => {
+  it('Returns correct array', () => {
+    const pagination = renderHook(() => usePagination(11, 10)).result.current;
+    expect(pagination.pages.map((p) => p.page)).toEqual([1, 2]);
+  });
+
+  it('Correctly updates the selected page', () => {
+    const pagination = renderHook(() => usePagination(11, 10)).result;
+    expect(pagination.current.pages[0].isSelected).toBe(true);
+
+    act(() => pagination.current.setPage(2));
+    expect(pagination.current.pages[1].isSelected).toBe(true);
+  });
+});
---
+++ client/src/app/hooks/usePagination/usePagination.ts
@@ -0,0 +1,26 @@
+import { useMemo, useState } from 'react';
+
+type Page = {
+  page: number;
+  isSelected: boolean;
+};
+
+export const usePagination = (totalItems: number, pageSize: number, startingPage: number = 1) => {
+  const [page, setPage] = useState(startingPage || 1);
+  const [pages, setPages] = useState([] as Page[]);
+  const count = Math.ceil(totalItems / pageSize);
+
+  useMemo(() => {
+    const pages: Page[] = [];
+    for (let i = 1; i <= count; i++) {
+      pages.push({
+        page: i,
+        isSelected: i === page,
+      });
+    }
+
+    setPages(pages);
+  }, [page, count]);
+
+  return { pages, page, setPage };
+};
