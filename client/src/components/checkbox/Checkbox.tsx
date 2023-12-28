175505f62253e0856675e0fa7b3dafc22fe40a7d
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2023-12-28 16:35:28 +0200

    Create Checkbox (#27)
    * create BaseCheckbox
    
    * create Checkbox component with size and colors
    
    * remove debug CSS
    
    * add positioning
    
    * add documentation
    
    * add an outline when focused
    
    * disable transition if not requested
    
    * run prettier

---
+++ client/src/components/checkbox/Checkbox.tsx
@@ -0,0 +1,44 @@
+import Icon from '../icon/Icon';
+import BaseCheckbox from '../internal/baseCheckbox/BaseCheckbox';
+import { CheckboxProps } from './types';
+import { MdDone } from 'react-icons/md';
+import './Checkbox.scss';
+import '@/styles/colors.scss';
+
+/**
+ * A styled checkbox whose color, size, positioning, and icon can be
+ * configured.
+ */
+function Checkbox(props: CheckboxProps): React.JSX.Element {
+  const {
+    className = '',
+    icon = <MdDone />,
+    color = 'primary',
+    side = 'right',
+    label,
+    size = 'medium',
+    onChange,
+    value,
+    checked,
+    name,
+    ...others
+  } = props;
+  return (
+    <label
+      className={`component-checkbox ${
+        checked ? 'checked' : 'unchecked'
+      } ${side} ${size} ${className}`}
+      {...others}
+    >
+      <span className="component-checkbox-label">{label}</span>
+      <div className={`checkbox ${size} ${color}`}>
+        <Icon size={size} className="component-checkbox-icon">
+          {icon}
+        </Icon>
+        <BaseCheckbox value={value} checked={checked} onChange={onChange} name={name} />
+      </div>
+    </label>
+  );
+}
+
+export default Checkbox;
