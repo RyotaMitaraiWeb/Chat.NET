6921d5f23a365dc585a8560bcc6da0ab243afa4f
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-29 00:42:17 +0200

    slight change in logic for uncontrolled radio

--- client/src/components/internal/baseRadio/BaseRadio.tsx
+++ client/src/components/internal/baseRadio/BaseRadio.tsx
@@ -5,7 +5,7 @@ function BaseRadio(props: BaseRadioProps): React.JSX.Element {
   const { className = '', defaultChecked, checked, ...others } = props;
   const checkedClassName = props.checked ? 'checked' : '';
   const classNames = `component-base-radio ${checkedClassName} ${className}`;
-  if (defaultChecked !== undefined) {
+  if (checked === undefined) {
     return (
       <input className={classNames} defaultChecked={defaultChecked} type="radio" {...others} />
     );
