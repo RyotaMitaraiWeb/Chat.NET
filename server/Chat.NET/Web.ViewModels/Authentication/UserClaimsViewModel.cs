ccd872fe7a5874c3d8b712e280135ed2f6efba94
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-31 13:18:59 +0200

    create UserClaimsViewModel and IJwtService

---
+++ server/Chat.NET/Web.ViewModels/Authentication/UserClaimsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.ViewModels.Authentication
+{
+    /// <summary>
+    /// Represents data for the user decoded from their JWT.
+    /// </summary>
+    public class UserClaimsViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public HashSet<string> Roles { get; set; } = [];
+    }
+}
