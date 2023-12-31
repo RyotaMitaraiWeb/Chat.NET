ec235d28dda3ca96c1ef3b7494901ab6b283a52a
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-01-01 00:49:10 +0200

    create UserService and implement Register

---
+++ server/Chat.NET/Tests.Unit/UserServiceTests.cs
@@ -0,0 +1,83 @@
+﻿using Common.Authentication;
+using Infrastructure.Postgres.Entities;
+using Microsoft.AspNetCore.Identity;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Services.Authentication;
+using Web.ViewModels.Authentication;
+
+namespace Tests.Unit
+{
+    public class UserServiceTests
+    {
+        public UserManager<ApplicationUser> UserManager { get; set; }
+        public UserService UserService { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            this.UserManager = UserManagerMock.Create();
+            this.UserService = new UserService(this.UserManager);
+        }
+
+        [Test]
+        public async Task Test_RegisterReturnsUserClaimsWhenSuccessful()
+        {
+            var register = new UserRegisterViewModel()
+            {
+                Username = "Test",
+                Password = "123456",
+            };
+
+            var appUser = new ApplicationUser()
+            {
+                UserName = register.Username,
+            };
+
+            this.UserManager
+                .CreateAsync(appUser, register.Password)
+                .ReturnsForAnyArgs(IdentityResult.Success);
+
+            this.UserManager
+                .AddToRoleAsync(appUser, Roles.User)
+                .Returns(IdentityResult.Success);
+
+            var result = await this.UserService.Register(register);
+            Assert.That(result?.Username, Is.EqualTo(register.Username));
+        }
+
+        [Test]
+        public async Task Test_RegisterReturnsNullIfUnsuccessful()
+        {
+            var register = new UserRegisterViewModel()
+            {
+                Username = "Test",
+                Password = "123456",
+            };
+
+            var appUser = new ApplicationUser()
+            {
+                UserName = register.Username,
+            };
+
+            this.UserManager
+                .CreateAsync(appUser, register.Password)
+                .ReturnsForAnyArgs(IdentityResult.Failed
+                (
+                    new IdentityError
+                    {
+                        Code = "Error",
+                        Description = "Error",
+                    }
+                ));
+
+            var result = await this.UserService.Register(register);
+            Assert.That(result, Is.Null);
+        }
+
+    }
+}
---
+++ server/Chat.NET/Web.Services/Authentication/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Common.Authentication;
+using Contracts;
+using Infrastructure.Postgres.Entities;
+using Microsoft.AspNetCore.Identity;
+using Web.ViewModels.Authentication;
+
+namespace Web.Services.Authentication
+{
+    public class UserService(UserManager<ApplicationUser> userManager) : IUserService
+    {
+        private readonly UserManager<ApplicationUser> userManager = userManager;
+
+        public async Task<UserClaimsViewModel?> Register(UserRegisterViewModel user)
+        {
+            var appUser = new ApplicationUser()
+            {
+                UserName = user.Username,
+            };
+
+            var registerResult = await userManager.CreateAsync(appUser, user.Password);
+            if (registerResult == null)
+            {
+                return null;
+            }
+
+            if (!registerResult.Succeeded)
+            {
+                return null;
+            }
+
+            await this.userManager.AddToRoleAsync(appUser, Roles.User);
+            return new UserClaimsViewModel()
+            {
+                Id = appUser.Id.ToString(),
+                Username = appUser.UserName,
+            };
+
+        }
+    }
+}
