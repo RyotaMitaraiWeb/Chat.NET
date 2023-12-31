d00f1a26f270c29d9dbb0969b2b1d5a1e62423eb
RyotaMitaraiWeb <titanium5624@gmail.com>
2023-12-31 12:30:34 +0200

    create constants for JWT claims

---
+++ server/Chat.NET/Common/Authentication/JwtClaims.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Authentication
+{
+    public static class JwtClaims
+    {
+        public const string Id = "id";
+        public const string Username = "username";
+        public const string Roles = "roles";
+    }
+}
