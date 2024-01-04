0743b1d1afbc88a4d71719c2508247d2fe5f40ff
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2024-01-04 23:02:53 +0200

    integrate SignalR in the project (#37)
    * configure CORS
    
    * install SignalR on the client
    
    * create an abstract class for the SignalR connection
    
    * make the SIgnalrConnection work with Authorized requests
    
    * create a SessionHubConnection
    
    * add a POC SignalR implementation

---
+++ client/src/signalr/SignalrConnection.ts
@@ -0,0 +1,87 @@
+import * as signalr from '@microsoft/signalr';
+
+interface ISignalrConnection<TClientMethods, TServerMethods> {
+  start: () => Promise<void>;
+  stop: () => Promise<void>;
+  invoke: (event: TServerMethods, ...args: Array<unknown>) => Promise<void>;
+  off: (event: TClientMethods, method?: (...args: unknown[]) => void) => void;
+}
+
+/**
+ * Encapsulates the ``signalr`` connection and provides
+ * support for strong typing. For each hub on the .NET backend,
+ * you should create a separate class that inherits this one.
+ */
+export abstract class SignalrConnection<TC extends string, TS extends string>
+  implements ISignalrConnection<TC, TS>
+{
+  protected connection: signalr.HubConnection;
+  private connectionIsActive = false;
+
+  /**
+   * @param hub A relative path to the hub (e.g. ``/chat``).
+   */
+  constructor(hub: string) {
+    this.connection = new signalr.HubConnectionBuilder()
+      .withUrl(`http://localhost:5000/${hub}`, {
+        accessTokenFactory() {
+          return localStorage.getItem('access_token') || '';
+        },
+        skipNegotiation: true,
+        transport: signalr.HttpTransportType.WebSockets,
+        withCredentials: true,
+      })
+      .withAutomaticReconnect()
+      .build();
+  }
+
+  /**
+   * Establishes a connection to the hub if one has not been established already
+   */
+  start() {
+    if (!this.connectionIsActive) {
+      this.connectionIsActive = true;
+      return this.connection.start();
+    }
+
+    return Promise.resolve(undefined);
+  }
+
+  /**
+   * Disconnects from the hub if there is an active connection to it.
+   */
+  stop() {
+    if (this.connectionIsActive) {
+      this.connectionIsActive = false;
+      return this.connection.stop();
+    }
+
+    return Promise.resolve(undefined);
+  }
+
+  invoke(event: TS, ...args: unknown[]): Promise<void> {
+    return this.connection.invoke(event, ...args);
+  }
+
+  on(event: TC, callback: (...args: unknown[]) => void): void {
+    this.connection.on(event, callback);
+  }
+
+  /**
+   * Deletes the specified handler from the connection. When you want
+   * to pass different behaviors upon the invocation of the same event,
+   * you should use named functions so that you can delete only that
+   * behavior.
+   * @param event
+   * @param method if using named methods, only the specified ``method``
+   * will be removed. If no ``method`` is specified, all handlers
+   * attached to the event will be removed.
+   */
+  off(event: TC, method?: (...args: unknown[]) => void) {
+    if (method) {
+      this.connection.off(event, method);
+    } else {
+      this.connection.off(event);
+    }
+  }
+}
---
+++ server/Chat.NET/Contracts/Hubs/ISessionClient.cs
@@ -0,0 +1,10 @@
+﻿using Web.ViewModels.User;
+
+namespace Contracts.Hubs
+{
+    public interface ISessionClient
+    {
+        Task SendSessionData(UserViewModel user);
+        Task EndSession();
+    }
+}
