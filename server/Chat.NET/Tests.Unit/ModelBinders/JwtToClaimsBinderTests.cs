219db233fac973f53dca9ac62cb0b618019f9c58
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2024-01-09 18:41:15 +0200

    Add a model binder to parse JWTs (#40)
    * create JwtToClaimsBinder
    
    * attach null if the JWT cannot be validated
    
    * add tests
    
    * add documentation

---
+++ server/Chat.NET/Tests.Unit/ModelBinders/JwtToClaimsBinderTests.cs
@@ -0,0 +1,51 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Primitives;
+using NSubstitute;
+using Web.ModelBinders;
+using Web.ViewModels.Authentication;
+
+namespace Tests.Unit.ModelBinders
+{
+    public class JwtToClaimsBinderTests
+    {
+        public IJwtService JwtService { get; set; }
+        public JwtToClaimsBinder Binder { get; set; }
+        public ModelBindingContext BindingContext { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.JwtService = Substitute.For<IJwtService>();
+            this.BindingContext = Substitute.For<ModelBindingContext>();
+            this.Binder = new JwtToClaimsBinder(this.JwtService);
+        }
+
+        [Test]
+        public async Task Test_BinderAttachesSuccessResultWhenJwtIsParsedCorrectly()
+        {
+            this.BindingContext.HttpContext.Request.Headers.Authorization.Returns((StringValues)(new string[] { "a" }));
+
+            var claims = new UserClaimsViewModel()
+            {
+                Id = "1",
+                Username = "Test",
+            };
+
+            this.JwtService.ExtractUserFromJWT("a").Returns(claims);
+            await this.Binder.BindModelAsync(this.BindingContext);
+            Assert.That(this.BindingContext.Result, Is.EqualTo(ModelBindingResult.Success(claims)));
+        }
+
+        [Test]
+        public async Task Test_BinderAttachesSuccessResultWithNullIfItCannotValidateTheJWT()
+        {
+            this.BindingContext.HttpContext.Request.Headers.Authorization.Returns((StringValues)(new string[] { string.Empty }));
+
+            
+            this.JwtService.ExtractUserFromJWT(string.Empty).Returns(x => throw new Exception());
+            await this.Binder.BindModelAsync(this.BindingContext);
+            Assert.That(this.BindingContext.Result, Is.EqualTo(ModelBindingResult.Success(null)));
+        }
+    }
+}
---
+++ server/Chat.NET/Web/ModelBinders/JwtToClaimsBinder.cs
@@ -0,0 +1,35 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Web.ModelBinders
+{
+    /// <summary>
+    /// Decodes the user from the JWT in the Authorization header and passes it to
+    /// the action. If the JWT cannot be decoded, null is attached in place of the parameter.
+    /// Because of this, the parameter receiving the claims must be marked as nullable
+    /// if used in a non-authorized route.
+    /// </summary>
+    /// <param name="jwtService"></param>
+    public class JwtToClaimsBinder(IJwtService jwtService) : IModelBinder
+    {
+        private readonly IJwtService jwtService = jwtService;
+
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+           
+           try
+            {
+                string? jwt = bindingContext.HttpContext?.Request.Headers.Authorization.First();
+                jwt ??= string.Empty;
+                var user = this.jwtService.ExtractUserFromJWT(jwt);
+                bindingContext.Result = ModelBindingResult.Success(user);
+            }
+            catch
+            {
+                bindingContext.Result = ModelBindingResult.Success(null);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
