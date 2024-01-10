b4a0649eb730b578c2f6d94c8c040c0760236e09
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-01-11 01:16:41 +0200

    update models

--- server/Chat.NET/Infrastructure/Redis/Models/UserSession.cs
+++ server/Chat.NET/Infrastructure/Redis/Models/UserSession.cs
@@ -1,9 +1,4 @@
 ﻿using Redis.OM.Modeling;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Redis.Models
 {
