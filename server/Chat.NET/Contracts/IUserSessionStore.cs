ddf23f42ab4ce6dbd18c8d41b466e2777d047874
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-01-12 00:47:49 +0200

    update IUserSessionStore

--- server/Chat.NET/Contracts/IUserSessionStore.cs
+++ server/Chat.NET/Contracts/IUserSessionStore.cs
@@ -5,20 +5,7 @@ namespace Contracts
 {
     public interface IUserSessionStore
     {
-        public Task<UserViewModel?> AddUser(UserClaimsViewModel claims, string connectionId);
-        /// <summary>
-        /// Removes the provided <paramref name="connectionId"/> from the user with the ID from the claims
-        /// </summary>
-        /// <param name="claims"></param>
-        /// <param name="connectionId"></param>
-        /// <returns></returns>
-        public Task<UserViewModel?> RemoveUser(UserClaimsViewModel claims, string connectionId);
-
-        /// <summary>
-        /// Deletes the user's session entirely.
-        /// </summary>
-        /// <param name="claims"></param>
-        /// <returns></returns>
+        public Task<UserViewModel?> AddUser(UserViewModel claims);
         public Task<UserViewModel?> RemoveUser(UserClaimsViewModel claims);
         public Task<UserViewModel?> GetUser(UserClaimsViewModel claims);
         public Task<UserViewModel?> UpdateRoles(UserClaimsViewModel claims, string[] roles); 
