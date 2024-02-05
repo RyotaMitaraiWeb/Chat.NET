c434f5abafd9fe12484412a9b34d1b5d0d0907e8
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-02-05 11:33:00 +0200

    introduce custom type for the debounced callback

--- client/src/hooks/useDebounce/useDebounce.ts
+++ client/src/hooks/useDebounce/useDebounce.ts
@@ -1,5 +1,7 @@
 import { useEffect, useMemo, useRef } from 'react';
 
+type DebouncedCallback = (...args: unknown[]) => void;
+
 function debounce<T extends (...args: unknown[]) => ReturnType<T>>(
   callback: T,
   timeout = 500,
@@ -14,17 +16,22 @@ function debounce<T extends (...args: unknown[]) => ReturnType<T>>(
   };
 }
 
-export const useDebounce = (callback: (...args: unknown[]) => void, timeout = 500) => {
-  const ref = useRef((...args: unknown[]) => {
-    args;
-  });
+/**
+ * If ``callback`` is called multiple times before the ``timeout`` is finished
+ * then only the last call will execute.
+ * @param callback
+ * @param timeout expressed in miliseconds, defaults to 500 (half a second)
+ * @returns a debounced function that calls ``callback``.
+ */
+export const useDebounce = (callback: DebouncedCallback, timeout = 500) => {
+  const ref = useRef<DebouncedCallback>();
 
   useEffect(() => {
     ref.current = callback;
   }, [callback]);
 
   const debouncedCallback = useMemo(() => {
-    const func = (...args: unknown[]) => {
+    const func: DebouncedCallback = (...args: unknown[]) => {
       ref.current?.(args);
     };
     return debounce(func, timeout);
