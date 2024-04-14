5c9089fc67111207e3f05f65f7af24aa4b697757
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-04-15 00:08:12 +0300

    add an endpoint for retrieving users based on roles

---
+++ server/Chat.NET/Tests.Unit/Controllers/RolesControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Common.Authentication;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Web.Controllers.Areas.Admin;
+using Web.ViewModels.User;
+
+namespace Tests.Unit.Controllers
+{
+    public class RolesControllerTests
+    {
+        public IRoleService RoleService { get; set; } = Substitute.For<IRoleService>();
+        public RolesController RolesController { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.RolesController = new RolesController(this.RoleService);
+        }
+
+        [Test]
+        public async Task Test_GetUsersOfRolesCorrectlyReturnsUsers()
+        {
+            string[] rolesInput = [Roles.Moderator, Roles.User];
+            var users = new List<UserViewModel>()
+            {
+                new() { Id = "1" }
+            };
+
+            this.RoleService.GetUsersOfRoles(rolesInput).Returns(users);
+
+            var result = await this.RolesController.GetUsersOfRoles(rolesInput);
+            var response = result as OkObjectResult;
+            var body = response?.Value as List<UserViewModel>;
+            Assert.That(body, Is.EqualTo(users));
+        }
+    }
+}
---
+++ server/Chat.NET/Web/Controllers/Areas/Admin/RolesController.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers.Areas.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController(IRoleService roleService) : BaseController
+    {
+        private readonly IRoleService roleService = roleService;
+        [Route("roles")]
+        public async Task<ActionResult> GetUsersOfRoles([FromQuery] string[] roles)
+        {
+            var users = await this.roleService.GetUsersOfRoles(roles);
+            return Ok(users);
+        }
+    }
+}
