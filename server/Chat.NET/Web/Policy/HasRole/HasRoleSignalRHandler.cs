c867ffcee9e01960368ef87ff5bf0e606e490d38
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-04-16 22:28:42 +0300

    refactor HasRoleSignalRHandler to be more testable

--- server/Chat.NET/Web/Policy/HasRole/HasRoleSignalRHandler.cs
+++ server/Chat.NET/Web/Policy/HasRole/HasRoleSignalRHandler.cs
@@ -17,14 +17,24 @@ namespace Web.Policy.HasRole
             string bearer = hubContext.Context.GetHttpContext()?.Request.Query["access_token"].ToString()
                 ?? string.Empty;
 
+            await this.Authorize(context, requirement, bearer);
+        }
+
+        /// <summary>
+        /// This method is publicly exposed for testing purposes
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="requirement"></param>
+        /// <param name="bearer"></param>
+        /// <returns></returns>
+        public async Task Authorize(AuthorizationHandlerContext context, HasRoleSignalRRequirement requirement, string bearer)
+        {
             var claims = this.jwtService.ExtractUserFromJWT(bearer);
             var user = (await this.userSessionStore.GetUser(claims))!;
 
             bool hasRole = HasRole(user.Roles, requirement.RequiredRoles);
             if (!hasRole)
             {
-                Console.WriteLine("Does not have role");
-
                 context.Fail();
                 return;
             }
