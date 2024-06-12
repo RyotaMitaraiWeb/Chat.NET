d4c0deab17b55a3c346e834b8ed863f50048b00c
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-06-12 15:50:04 +0300

    create an enum for update role results

---
+++ server/Chat.NET/Common/Enums/RoleUpdateResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Common.Enums
+{
+    public enum RoleUpdateResult
+    {
+        GeneralFail,
+        Success,
+        RoleNotAvailableForUpdate,
+        RoleAlreadyGiven,
+        RoleNotGiven,
+        UserDoesNotExist,
+    }
+}
--- server/Chat.NET/Common/ErrorMessages/RoleErrorMessages.cs
+++ server/Chat.NET/Common/ErrorMessages/RoleErrorMessages.cs
@@ -1,13 +1,41 @@
-﻿namespace Common.ErrorMessages
+﻿using Common.Enums;
+
+namespace Common.ErrorMessages
 {
     public static class RoleErrorMessages
     {
         public static class UpdateFailed
         {
             public const string UserDoesNotExist = "This user does not exist";
+            public const string GeneralFail = "Could not update the user's roles, please try again later!";
             public static string RoleNotAvailableForUpdate(string role) => $"Role \"{role}\" cannot be given to or removed from users";
             public static string UserAlreadyHasRole(string role) => $"This user already has role \"{role}\"";
             public static string UserDoesNotHaveRole(string role) => $"This user does not have role \"{role}\"";
+        }
+
+        private static readonly Dictionary<RoleUpdateResult, string> StaticErrors = new()
+        {
+            { RoleUpdateResult.Success, string.Empty },
+            { RoleUpdateResult.GeneralFail, UpdateFailed.GeneralFail },
+            { RoleUpdateResult.UserDoesNotExist, UpdateFailed.UserDoesNotExist },
+        };
+
+        private static readonly Dictionary<RoleUpdateResult, Func<string, string>> DynamicErrors = new()
+        {
+            { RoleUpdateResult.RoleNotAvailableForUpdate, UpdateFailed.RoleNotAvailableForUpdate },
+            { RoleUpdateResult.RoleAlreadyGiven, UpdateFailed.UserAlreadyHasRole },
+            { RoleUpdateResult.RoleNotGiven, UpdateFailed.UserDoesNotHaveRole },
+        };
+
+        public static string GenerateErrorMessage(string role, RoleUpdateResult result)
+        {
+            if (StaticErrors.TryGetValue(result, out string? value))
+            {
+                return value;
+            }
+
+            var fn = DynamicErrors[result];
+            return fn(role);
         }
 
         public static string RoleDoesNotExist(string role) => $"Role \"{role}\" does not exist!";
