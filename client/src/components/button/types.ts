0995cdabe4f3816d360560322f0958604cb20c28
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-06-15 01:28:32 +0300

    add an option for a transparent button background color

--- client/src/components/button/types.ts
+++ client/src/components/button/types.ts
@@ -2,6 +2,6 @@ import { BaseButtonWithIconProps } from '../types/BaseButton';
 import { palette, size } from '../types/options';
 
 export interface ButtonProps extends BaseButtonWithIconProps {
-  color?: palette;
+  color?: palette | 'transparent';
   size?: size;
 }
