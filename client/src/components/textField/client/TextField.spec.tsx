255b36e1c956f5ad8749ffe1e249ef443b58aff3
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-06-24 23:20:04 +0300

    fix unit tests

--- client/src/components/textField/client/TextField.spec.tsx
+++ client/src/components/textField/client/TextField.spec.tsx
@@ -3,9 +3,14 @@ import TextField from './TextField';
 
 describe('TextField (client)', () => {
   describe('Placeholder', () => {
-    it('Displays correct placeholder when passed one', async () => {
-      render(<TextField value="a" onChange={() => {}} placeholder="Test" />);
+    it('Displays correct placeholder when passed one and the field is required', async () => {
+      render(<TextField value="a" required onChange={() => {}} placeholder="Test" />);
       await screen.findByPlaceholderText('Test*');
+    });
+
+    it('Displays correct placeholder when passed one and the field is not required', async () => {
+      render(<TextField value="a" onChange={() => {}} placeholder="Test" />);
+      await screen.findByPlaceholderText('Test');
     });
   });
 });
--- client/src/components/textField/server/TextField.spec.tsx
+++ client/src/components/textField/server/TextField.spec.tsx
@@ -3,9 +3,14 @@ import TextField from './TextField';
 
 describe('TextField (server)', () => {
   describe('Placeholder', () => {
-    it('Displays correct placeholder when passed one', async () => {
-      render(<TextField placeholder="Test" />);
+    it('Displays correct placeholder when passed one and the field is required', async () => {
+      render(<TextField placeholder="Test" required />);
       await screen.findByPlaceholderText('Test*');
+    });
+
+    it('Displays correct placeholder when passed one and the field is not required', async () => {
+      render(<TextField placeholder="Test" />);
+      await screen.findByPlaceholderText('Test');
     });
   });
 });
