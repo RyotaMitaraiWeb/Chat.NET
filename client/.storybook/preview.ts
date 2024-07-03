9ebba0e3de353de1bdeef7466f339ee158115f32
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-07-03 22:50:02 +0300

    import normalize in Storybook

--- client/.storybook/preview.ts
+++ client/.storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react';
-import '../src/app/globals.scss'
+import '../src/app/globals.scss';
+import 'normalize.css';
+
 const preview: Preview = {
   parameters: {
     controls: {
