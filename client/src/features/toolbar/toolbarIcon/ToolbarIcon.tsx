eef8c5f7492b29d0b54cecefc45834f19ea3a2de
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-08-01 23:12:47 +0300

    create menu button

---
+++ client/src/features/toolbar/toolbarIcon/ToolbarIcon.tsx
@@ -0,0 +1,39 @@
+import IconButton from '@/components/button/iconButton/IconButton';
+import { ExtraSmallScreen, LargeScreen } from '@/components/screen/Screen';
+
+type ToolbarIconProps = {
+  children: React.ReactNode;
+  onClick?: () => void;
+  href?: string;
+};
+
+function ToolbarIcon(props: ToolbarIconProps): React.JSX.Element {
+  return (
+    <>
+      <ExtraSmallScreen to="large">
+        <IconButton
+          className="toolbar-icon"
+          onClick={props.onClick}
+          href={props.href}
+          variant="text"
+          size="medium"
+        >
+          {props.children}
+        </IconButton>
+      </ExtraSmallScreen>
+      <LargeScreen>
+        <IconButton
+          className="toolbar-icon"
+          onClick={props.onClick}
+          href={props.href}
+          variant="text"
+          size="large"
+        >
+          {props.children}
+        </IconButton>
+      </LargeScreen>
+    </>
+  );
+}
+
+export default ToolbarIcon;
