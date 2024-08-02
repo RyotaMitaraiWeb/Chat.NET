367542852c13da05c421b41f5aec6b47122b4e1b
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-08-02 23:40:01 +0300

    save the token in localStorage

--- client/src/app/auth/register/page.tsx
+++ client/src/app/auth/register/page.tsx
@@ -25,6 +25,8 @@ export default function Page() {
     });
 
     if (res.ok) {
+      const data = await res.json();
+      localStorage.setItem('access_token', data.token);
       router.push('/');
     }
   }
