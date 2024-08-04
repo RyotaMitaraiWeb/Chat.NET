81750132ac4486d2aa0dd8debd477325043ba4dc
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-08-05 02:25:45 +0300

    connect to the hub only after a successful authentication

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,16 +3,14 @@
 import { SessionContext } from '@/context/session/SessionContext';
 import { useInit } from '@/hooks/useInit/useInit';
 import { authService } from '@/services/authService';
-import { chatHubConnection } from '@/signalr/ChatHubConnection';
 import { use } from 'react';
 
 function App({ children }: { children: React.ReactNode }): React.JSX.Element {
   const { setUser, restartUser } = use(SessionContext);
   useInit(() => {
-    chatHubConnection
-      .start()
-      .then(() => authService.startSession(setUser))
-      .catch(() => restartUser());
+    if (localStorage.getItem('access_token')) {
+      authService.startSession(setUser).catch(restartUser);
+    }
   });
 
   return <main>{children}</main>;
--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -5,8 +5,10 @@ export const authService = {
    * Invokes a SignalR event to start the user's session and registers
    * an event listener for the session's start.
    */
-  startSession(callback: (data: never) => void): Promise<void> {
-    chatHubConnection.on('SendSessionData', callback);
-    return chatHubConnection.invoke('StartSession');
+  async startSession(callback: (data: never) => void): Promise<void> {
+    return chatHubConnection.start().then(() => {
+      chatHubConnection.on('SendSessionData', callback);
+      return chatHubConnection.invoke('StartSession');
+    });
   },
 };
