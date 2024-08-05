27be860505b1433ed4ad8d203538609f1da38f43
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-08-05 14:52:22 +0300

    update authService

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -1,11 +1,13 @@
 import { chatHubConnection } from '@/signalr/ChatHubConnection';
+import { UserClaims } from '@/types/auth';
 
 export const authService = {
   /**
-   * Invokes a SignalR event to start the user's session and registers
+   * Starts a connection with the socket server,
+   * invokes a SignalR event to start the user's session and registers
    * an event listener for the session's start.
    */
-  async startSession(callback: (data: never) => void): Promise<void> {
+  async startSession(callback: (data: UserClaims) => void): Promise<void> {
     return chatHubConnection.start().then(() => {
       chatHubConnection.on('SendSessionData', callback);
       return chatHubConnection.invoke('StartSession');
