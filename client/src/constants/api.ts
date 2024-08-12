c8383ab53aa995a8a08e4ae6b1f7f4cbffd8d09a
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-08-13 01:02:57 +0300

    update api

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,10 +1,18 @@
-const apiUrl = 'http://localhost:5000';
+const apiUrl = process.env.NEXT_API_URL;
 const auth = `${apiUrl}/auth`;
+const chat = `${apiUrl}/chat`;
 
 export const api = {
   auth: {
     register: `${auth}/register`,
     login: `${auth}/login`,
     usernameExists: (username: string) => `${auth}/username-exists/${username}`,
+  },
+  chat: {
+    get: (id: string | number) => `${chat}/${id}`,
+    create: chat,
+    update: (id: string | number) => `${chat}/${id}`,
+    delete: (id: string | number) => `${chat}/${id}`,
+    search: chat,
   },
 };
