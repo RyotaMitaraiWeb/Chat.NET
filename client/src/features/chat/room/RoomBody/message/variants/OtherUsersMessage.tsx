bb6a0d3ff53ee7d5b70a27b05b132c488a1cd99d
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-08-13 18:50:17 +0300

    format date and change padding

--- client/src/features/chat/room/RoomBody/message/variants/OtherUsersMessage.tsx
+++ client/src/features/chat/room/RoomBody/message/variants/OtherUsersMessage.tsx
@@ -4,9 +4,18 @@ import Link from 'next/link';
 
 function OtherUsersMessage(props: ChatRoomMessageProps): React.JSX.Element {
   const message = props.message;
+  const date = new Date(props.message.date).toLocaleString('en-us', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    hour12: false,
+    minute: 'numeric',
+  });
+
   return (
     <article className="chat-room-message other-user">
-      <time className="chat-room-message-date">{message.date}</time>
+      <time className="chat-room-message-date">{date}</time>
       <div className="chat-room-message-content">
         <div className="chat-room-message-sender-username">
           <Link className="chat-room-message-sender-link" href={`/profile/${message.sender.id}`}>
--- client/src/features/chat/room/RoomBody/message/variants/OwnMessage.tsx
+++ client/src/features/chat/room/RoomBody/message/variants/OwnMessage.tsx
@@ -3,9 +3,18 @@ import { ChatRoomMessageProps } from '../types';
 
 function OwnMessage(props: ChatRoomMessageProps): React.JSX.Element {
   const message = props.message;
+  const date = new Date(props.message.date).toLocaleString('en-us', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    hour12: false,
+    minute: 'numeric',
+  });
+
   return (
     <article className="chat-room-message own">
-      <time className="chat-room-message-date">{message.date}</time>
+      <time className="chat-room-message-date">{date}</time>
       <div className="chat-room-message-content">
         <span>{message.content}</span>
       </div>
