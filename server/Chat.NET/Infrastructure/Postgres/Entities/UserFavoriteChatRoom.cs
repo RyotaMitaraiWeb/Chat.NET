b516e2932dc671f584399c04472799af39bf8a15
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-08-20 02:22:02 +0300

    define favorite chat rooms in the database

---
+++ server/Chat.NET/Infrastructure/Postgres/Entities/UserFavoriteChatRoom.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Postgres.Entities
+{
+    public class UserFavoriteChatRoom
+    {
+        public int Id { get; set; }
+        public ApplicationUser User { get; set; } = null!;
+        [ForeignKey(nameof(User))]
+        public Guid UserId { get; set; }
+        public ChatRoom ChatRoom { get; set; } = null!;
+        [ForeignKey(nameof(ChatRoom))]
+        public int ChatRoomId { get; set; }
+    }
+}
