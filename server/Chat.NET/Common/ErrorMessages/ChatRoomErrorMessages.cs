abf2c0b99ae3872334cd3462eb34e68a231cc64b
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-09-10 17:17:03 +0300

    create unique tags validator

---
+++ server/Chat.NET/Common/ErrorMessages/ChatRoomErrorMessages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ErrorMessages
+{
+    public static class ChatRoomErrorMessages
+    {
+        public static class Tags
+        {
+            public const string ContainDuplicateValues = "The tags cannot contain duplicate values (case insensitive)";
+        }
+    }
+}
---
+++ server/Chat.NET/Web.Validators/UniqueTags.cs
@@ -0,0 +1,30 @@
+﻿using Common.ErrorMessages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Validators
+{
+    /// <summary>
+    /// Performs a case insensitive check as to whether the collection of tags contains
+    /// duplicate values. Tags should not have duplicate values.
+    /// </summary>
+    public class UniqueTags : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is not IEnumerable<string> tags)
+            {
+                return new ValidationResult("Tags must be provided as an enumerable of strings");
+            }
+
+            IEnumerable<string> normalizedTags = tags.Select(t => t.ToUpper());
+            bool onlyUniqueTags = normalizedTags.Distinct().Count() == normalizedTags.Count();
+
+            if (onlyUniqueTags)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(ChatRoomErrorMessages.Tags.ContainDuplicateValues);
+        }
+    }
+}
